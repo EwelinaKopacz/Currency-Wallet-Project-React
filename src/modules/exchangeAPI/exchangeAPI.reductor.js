361e90762f28372409e41cc15ac4067111dae869
Ewelina <ewelina@ekopacz.pl>
2022-05-29 18:51:59 +0200

    update

--- src/modules/exchangeAPI/exchangeAPI.reductor.js
+++ src/modules/exchangeAPI/exchangeAPI.reductor.js
@@ -1,3 +1,4 @@
+import { act } from 'react-dom/test-utils';
 import types from './exchangeAPI.types';
 
 const initState = [];
@@ -13,20 +14,39 @@ const exchageReductor = (state=initState,action) => {
         case types.UPDATE_LATEST_EXCHANGE_RATE:
             const {key} = action.payload
             // znajdz el z takim kluczem
-            const findItem = state.find((item) => item[key] === key) 
+
+            // ---> SZUKAJ PO KLUCZU ELE DO ZAKTUALIZOWANIA
+            //const findItem = state.find((item) => item[key] === key) 
 
             // zaktualizuj el o takim kluczu
-            let newState = state
-            newState = state.map((item) => {
-                console.log(item)
-                if(item === findItem){
-                    return {item:action.payload}
-                } return item
+            // let newState = state
+            // newState = state.map((item) => {
+            //     console.log(item)
+            //     if(item === findItem){
+            //         return {item:action.payload}
+            //     } return item
 
-            })
+            // })
             // ustaw nowy state
+
+            // --->  SZUKAJ PO INDEKSIE ELE DO ZAKTUALIZOWANIA
+            // const findItem = state.findIndex((item) => item[key] === key);
+            // const updateState = state.map((el,index) => {
+            //     if(el[index]=== findItem){
+            //         return {el:action.payload}
+            //     }
+            // })
+            //const updateState = {...action.payload, state[findIndex]:action.payload}
+
+
+            // ---> USUN ITEM O TAKIM KLUCZU I DODAJ NOWY PAYLOAD
+            const updateState = state.filter((item) => item[key] !== key )
+            updateState.push(action.payload)
+
             return [
-                newState
+                //newState
+                ...updateState,
+                //action.payload
             ]
         default:
             return state;
